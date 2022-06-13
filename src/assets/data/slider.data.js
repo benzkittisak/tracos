9f5521c8855e15d8604c537246e5aab91a3a43fd
benzkittisak <benzkittisak@stv.ac.th>
2022-06-13 22:11:59 +0700

    update images url

--- src/assets/data/slider.data.js
+++ src/assets/data/slider.data.js
@@ -3,21 +3,21 @@ const SLIDER_DATA = [
         id:1,
         title:'เมืองหลวงของประเทศไทย',
         location:'กรุงเทพมหานคร',
-        image:'https://i3.fpic.cc/file/img-b1/2022/01/05/bangkok-thailand-skyline-2021-08-26-18-13-18-utc.jpg'
+        image:'https://sv1.picz.in.th/images/2022/06/13/V4FrzN.jpg'
     }
     ,
     {
         id:2,
         title:'เมืองท่องเที่ยวพัทยา',
         location:'พัทยา',
-        image:'https://i3.fpic.cc/file/img-b1/2022/01/05/building-sea-and-beach-of-pattaya-city-in-day-tim-2021-08-31-22-42-21-utc.jpg'
+        image:'https://sv1.picz.in.th/images/2022/06/13/V4Fa1n.jpg'
     }
     ,
     {
         id:3,
         title:'ไม่ลองไม่ได้กับผัดไทย',
         location:'ผัดไทย',
-        image:'https://i3.fpic.cc/file/img-b1/2022/01/05/pad-thai-2021-08-30-16-16-41-utc.jpg'
+        image:'https://sv1.picz.in.th/images/2022/06/13/V4FJk2.jpg'
     }
 ]
 
